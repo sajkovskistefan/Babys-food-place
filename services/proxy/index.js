3c93dcff81894aa88e8208632399d131ec532769
Stefan Sajkovski <sajko@Stefans-MacBook-Pro.local>
2021-03-08 23:34:51 +0100

    aaaahhhhhhhhaaaaaahhhhhhh

--- services/proxy/index.js
+++ services/proxy/index.js
@@ -35,7 +35,7 @@ app.use(
     '/api/v1/blogposts',
     proxy(
         'http://localhost:10004',
-        { proxyReqPathResolver: (req) => `http://localhost:10004/api/v1/blogposts${req.url}` }
+        { proxyReqPathResolver: (req) => cors() `http://localhost:10004/api/v1/blogposts${req.url}` }
     )
 );
 
